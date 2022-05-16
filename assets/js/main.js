f2f58bb2f7b8930ff3e184db9f078de7b77b96a0
misaraadel <misaraadel@gmail.com>
2022-05-16 15:23:41 +0200

    edit website

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,19 @@ $(document).ready(function () {
         }, 100);
     });
 
+    $(".show_hide_password .show_pass").on('click', function(event) {
+        event.preventDefault();
+        if($(this).siblings("input").attr("type") == "text"){
+            $(this).siblings("input").attr('type', 'password');
+            $(this).addClass( "fa-eye-slash" );
+            $(this).removeClass( "fa-eye" );
+        }else if($(this).siblings("input").attr("type") == "password"){
+            $(this).siblings("input").attr('type', 'text');
+            $(this).removeClass( "fa-eye-slash" );
+            $(this).addClass( "fa-eye" );
+        }
+    });
+
     $('.hamburger').click(function(){
         $('.hamburger').toggleClass('active');
         $('.navbar-nav').toggleClass('active-nav');
@@ -46,13 +59,77 @@ $(document).ready(function () {
         autoplayHoverPause: true,
         responsive:{
             0:{
-                items:1
+                items:2,
+                margin: 10,
             },
             600:{
                 items:2
             },
             1000:{
                 items:5
+            }
+        }
+    });
+
+    $('.owl-features').owlCarousel({
+        rtl: true,
+        loop:true,
+        margin: 20,
+        nav:false,
+        dots: true,
+        autoplay: true,
+        touchDrag  : true,
+		mouseDrag  : true,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplayTimeout: 6000,
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+
+    $('.owl-ideas').owlCarousel({
+        rtl: true,
+        loop:true,
+        margin: 0,
+        nav:false,
+        dots: true,
+        autoplay: true,
+        touchDrag  : true,
+		mouseDrag  : true,
+		autoWidth: false,
+		animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        autoplayTimeout: 6000,
+		smartSpeed: 2000,
+		dragEndSpeed: 2000,
+		slidSpeed: 900,
+        paginationSpeed: 900,
+        autoplayHoverPause: true,
+        responsive:{
+            0:{
+                items:2,
+                margin: 10,
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:2
             }
         }
     });
